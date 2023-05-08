43110671f293f32f4fbd7535a105aa18252bfba9
dong-3-hoon <tpehd1009@gmail.com>
2023-05-08 17:53:19 +0900

    230508

---
+++ daily/230508/baek_4949.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string>
+#include<stack>
+using namespace std;
+int main(){
+    char a;
+    while(1){
+        cin>>a;
+        string lst;
+        if(a=='.'){
+            break;
+        }
+        else{
+            getline(cin, lst);
+            for(int i=0; i<lst.length();i++){
+                if(lst[i]=='(' ||lst[i]=='['){
+                    
+                }
+            }
+        }
+    }
+
+}
