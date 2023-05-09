da94c9cffc6fbea550c1acd9e88496b8afd0f425
dong-3-hoon <tpehd1009@gmail.com>
2023-05-09 17:46:43 +0900

    230509

--- daily/230509/baek_11866.cpp
+++ daily/230509/baek_11866.cpp
@@ -3,6 +3,54 @@
 using namespace std;
 int main(){
     int len, num;
+    queue<int> q;
     cin>>len>>num;
-    
+
+    int lst[len];
+    int stk=0;
+    int cnt=0;
+    for(int i=1;i<len+1;i++){
+        q.push(i);
+    }
+    while(1){
+        if(q.size()==0){
+            break;
+        }
+        if(cnt==num){
+            lst[stk]=q.front();
+            q.pop();
+            stk++;
+            cnt=0;
+        }
+        else{
+            q.push(q.front());
+            q.pop();
+            }
+        cnt++;
+    }
+    cout<<"<";
+    for(int i=0;i<len;i++){
+        if(len==1){
+            cout<<len;
+        }
+        else{
+            if(lst[i]==1){
+                if(i==len-1){
+                    cout<<len;
+                }
+                else{
+                    cout<<len<<", ";
+                }
+            }
+            else{
+                if(i==len-1){
+                    cout<<lst[i]-1;
+                }
+                else{
+                    cout<<lst[i]-1<<", ";
+                }
+            }
+        }
+    }
+    cout<<">";
 }
---
+++ daily/230509/baek_1966.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<string>
+#include<queue>
+using namespace std;
+int main(){
+    int num;
+    cin>>num;
+    for(int i=0;i<num;i++){
+        int a, b, c;
+        int cnt = 9;
+        int stk = 0;
+        queue<int> q;
+        cin>>a>>b;
+        for(int j=0;j<a;j++){
+            cin>>c;
+            q.push(c);
+        }
+    }
+}
